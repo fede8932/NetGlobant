abb65b578673772007fc7baabac4dd2051b52957
fede8932 <federi.angel@gmail.com>
2022-03-23 16:08:46 -0300

    redux de usuario y geolocalizacion

---
+++ src/states/geoLocalizacion.js
@@ -0,0 +1,10 @@
+import { createReducer , createAction} from "@reduxjs/toolkit";
+
+export const setPosition = createAction("GEOLOCALIZACION");
+
+const geoReducer = createReducer([], {
+  [setPosition]: (state, action) => action.payload
+})
+
+
+export default geoReducer;
---
+++ src/states/user.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { createReducer , createAsyncThunk} from "@reduxjs/toolkit";
+
+export const sendLogin = createAsyncThunk("LOGIN", (dataUser) => {
+  return axios.post("/api/auth/login" , {
+  email: dataUser.email,
+  contraseña: dataUser.contraseña
+  }).then((res) => res.data);
+});
+export const effectLogin = createAsyncThunk("PERSISTENCIA", () => {
+  return axios.get("/api/auth/me").then((res) => res.data);
+});
+
+const userReducer = createReducer([], {
+  [sendLogin.fulfilled]: (state, action) => action.payload,
+  [effectLogin.fulfilled]: (state, action) => action.payload,
+})
+
+
+export default userReducer;
