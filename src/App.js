53d3d1b72cdee3b0c72c233a8d919fc797719c6f
DoloresPolito <dolores.polito@gmail.com>
2022-03-25 17:36:51 -0300

    algunos cambios

--- src/App.js
+++ src/App.js
@@ -1,40 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from "react";
 
-import { setPosition } from './states/geoLocalizacion';
-import { effectLogin } from './states/user';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { ThemeProvider } from "@material-ui/styles";
+import { createTheme } from "@material-ui/core/styles";
+import { setPosition } from "./states/geoLocalizacion";
+import { effectLogin } from "./states/user";
 
+import Login from "./components/Login";
+import ClientForm from "./components/ClientFrom";
+import SecurityForm from "./components/SecurityForm";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#F1EFEC",
+      main: "#BFBDB8",
+    },
+    secondary: {
+      main: "#F7C113",
+    },
+  },
+});
 
 function App() {
-  const dispatch = useDispatch()
-  useEffect(()=>{
-    navigator.geolocation.getCurrentPosition((data) => dispatch(setPosition([data.coords.latitude , data.coords.longitude])), (err) => console.error(err))
-  },[])
-  useEffect(()=>{
-    dispatch(effectLogin()).then(res=>console.log(res)).catch(err=>console.log(err))
-  },[])
-  const ubi=useSelector(state=>state.ubicacion)
+  const dispatch = useDispatch();
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (data) =>
+        dispatch(setPosition([data.coords.latitude, data.coords.longitude])),
+      (err) => console.error(err)
+    );
+  }, []);
+  useEffect(() => {
+    dispatch(effectLogin())
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
+  const ubi = useSelector((state) => state.ubicacion);
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>{`latitud ${ubi[0]} Longitud ${ubi[1]}`}</h2>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <Login />
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
