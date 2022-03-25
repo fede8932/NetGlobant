cbf7c05464ef88f7779380e6b5b3dd14ff3ac50f
fede8932 <federi.angel@gmail.com>
2022-03-25 10:01:55 -0300

    modificaciones

--- src/App.js
+++ src/App.js
@@ -2,40 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { Routes , Route } from 'react-router-dom';
 
 import { setPosition } from './states/geoLocalizacion';
 import { effectLogin } from './states/user';
+import Login from './components/Login'
+import ClientForm from './components/ClientFrom'
+import SecurityForm from './components/SecurityForm'
 
 
 function App() {
   const dispatch = useDispatch()
   useEffect(()=>{
     navigator.geolocation.getCurrentPosition((data) => dispatch(setPosition([data.coords.latitude , data.coords.longitude])), (err) => console.error(err))
-  },[])
-  useEffect(()=>{
     dispatch(effectLogin()).then(res=>console.log(res)).catch(err=>console.log(err))
   },[])
-  const ubi=useSelector(state=>state.ubicacion)
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>{`latitud ${ubi[0]} Longitud ${ubi[1]}`}</h2>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>NAVBAR</p>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/client" element={<ClientForm />} />
+        <Route path="/security" element={<SecurityForm />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+
+
+
