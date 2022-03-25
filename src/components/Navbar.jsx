5fdb61b73ce42c3d36f53ca50cd6dfc57e669ff2
Alexis Urquiza <urquizaalexis22@gmail.com>
2022-03-25 17:35:59 -0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,41 +3,57 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
+import Button from "@material-ui/core/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from "@mui/styles";
 import { useNavigate } from "react-router-dom";
+import { spacing } from "@mui/system";
+import { red } from "@material-ui/core/colors";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "../utils/themeConfig";
+
+const useStyle = makeStyles((theme) => ({
+  boton: {
+    color:"#212121",
+    backgroundColor: red
+},
+}));
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const { boton } = useStyle();
 
   const handleClick = () => {
     navigate("/login");
   };
-  
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon></MenuIcon>
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            NetGlobal
-          </Typography>
-          <Button onClick={handleClick} color="inherit">
-            Login
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              className={boton}
+            >
+              <MenuIcon></MenuIcon>
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              NetGlobal
+            </Typography>
+            <Button onClick={handleClick} color="inherit">
+              Login
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Button color="secondary">boton</Button>
+      </Box>
+    </ThemeProvider>
   );
 };
 
---
+++ src/utils/themeConfig.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@material-ui/core/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#F1EFEC",
+      main: "#BFBDB8",
+    },
+    secondary: {
+      main: "#F7C113",
+    },
+  },
+});
+export default theme;
