0df9c0a0fc162e2437fa67e75f67b947d1b8a021
Alexis Urquiza <urquizaalexis22@gmail.com>
2022-03-28 18:31:13 -0300

    merge

--- api/routes/adminRoutes.js
+++ api/routes/adminRoutes.js
@@ -1,40 +1,29 @@
-const adminRouter = require("express").Router()
-const AdminController= require("../controllers/AdminController")
+const adminRouter = require("express").Router();
+const AdminController = require("../controllers/AdminController");
 
-adminRouter.get("/clients", AdminController.getAllClients)
-adminRouter.get("/clients/:id", AdminController.getOneClient)
-adminRouter.get("/securities", AdminController.getAllSecurities)
-adminRouter.get("/securities/:id", AdminController.getOneSecurity)
-adminRouter.get("/office",AdminController.getAllOffice)
-adminRouter.get("/office/:id", AdminController.getOneOffice)
-adminRouter.get("/calendar/office/:id", AdminController.getOfficeCalendar)
+adminRouter.get("/clients", AdminController.getAllClients);
+adminRouter.get("/clients/:id", AdminController.getOneClient);
+adminRouter.get("/securities", AdminController.getAllSecurities);
+adminRouter.get("/securities/:id", AdminController.getOneSecurity);
+adminRouter.get("/office", AdminController.getAllOffice);
+adminRouter.get("/office/:id", AdminController.getOneOffice);
+adminRouter.get("/calendar/office/:id", AdminController.getOfficeCalendar);
 
+adminRouter.post("/add/office/security", AdminController.addSecurity);
+adminRouter.post("/add/office", AdminController.addOffice);
+adminRouter.post("/add/client", AdminController.addClient);
+adminRouter.post("/add/Calendar/office", AdminController.addSchedule);
 
-adminRouter.post("/add/office/security", AdminController.addSecurity)
-adminRouter.post("/add/office", AdminController.addOffice)
-adminRouter.post("/add/client",AdminController.addClient)
-adminRouter.post("/add/Calendar/office", AdminController.addSchedule)
+adminRouter.delete("/remove/security/:id", AdminController.removeSecurity);
+adminRouter.delete("/remove/client/:id", AdminController.removeClient);
+adminRouter.delete("/remove/office/:id", AdminController.removeOffice);
+adminRouter.delete(
+  "/remove/calendar/office/:id",
+  AdminController.removeSchedule
+);
 
+adminRouter.put("/edit/office/:id", AdminController.editOffice);
+adminRouter.put("/edit/security/:id", AdminController.editSecurity);
+adminRouter.put("/edit/client/:id", AdminController.editClient);
 
-adminRouter.delete("/remove/security/:id", AdminController.removeSecurity)
-adminRouter.delete("/remove/client/:id",AdminController.removeClient)
-adminRouter.delete("/remove/office/:id",AdminController.removeOffice)
-adminRouter.delete("/remove/calendar/office/:id", AdminController.removeSchedule)
-
-adminRouter.put("/edit/office/:id", AdminController.editOffice)
-adminRouter.put("/edit/security/:id", AdminController.editSecurity)
-adminRouter.put("/edit/client/:id",AdminController.editClient)
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports= adminRouter
+module.exports = adminRouter;
