872ad6822f9ee9c6de3607a31fd8394ae893e42e
Alexis Urquiza <urquizaalexis22@gmail.com>
2022-03-28 18:45:11 -0300

    merge

--- api/controllers/Auth.js
+++ api/controllers/Auth.js
@@ -8,16 +8,11 @@ class AuthController {
     return res.sendStatus(201);
   }
 
-<<<<<<< HEAD
-  static login(req, res) {
-    return res.send(req.user);
-=======
-  static async login(req, res){
+  static async login(req, res) {
     const { error, response } = await UserServices.login(req.body);
 
     if (error) return res.status(401).send("Email or password is wrong");
     return res.send(response);
->>>>>>> 7ad5b02bd6c9c56072d1e0198ba5f56900bbb5db
   }
 
   static logout(req, res) {
