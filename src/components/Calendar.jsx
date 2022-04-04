447d4bc0372bdd01d306990c808f5d1f7d0ed555
Alexis Urquiza <urquizaalexis22@gmail.com>
2022-04-04 18:15:00 -0300

    merge

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,26 +1,25 @@
-import Calendar from 'react-calendar';
-import { useState } from 'react';
-import 'react-calendar/dist/Calendar.css';
-
+import Calendar from "react-calendar";
+import { useState } from "react";
+import "react-calendar/dist/Calendar.css";
 
 const CalendarComponent = () => {
-    const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date());
 
-    return (
-      <div className='app '>
-        <h1 className='text-center'>React Calendar</h1>
-        <div className='calendar-container' style={{marginLeft: "550px", marginTop:"80px", size: "150px"}}>
-          <Calendar  onChange={setDate} value={date} />
-          {console.log(date)}
-        </div>
-        <p className='text-center'>
-          <span className='bold'>Selected Date:</span>{' '}
-          {date.toDateString()}
-        </p>
+  return (
+    <div className="app ">
+      <h1 className="text-center">React Calendar</h1>
+      <div
+        className="calendar-container"
+        style={{ marginLeft: "550px", marginTop: "80px", size: "150px" }}
+      >
+        <Calendar onChange={setDate} value={date} />
+        {console.log(date)}
       </div>
-    );
-
-}
-
+      <p className="text-center">
+        <span className="bold">Selected Date:</span> {date.toDateString()}
+      </p>
+    </div>
+  );
+};
 
 export default CalendarComponent;
