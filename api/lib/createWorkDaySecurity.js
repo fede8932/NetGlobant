0e30011e161a83dfe85a856f8a10f2bf7b0a31ee
Marisol Yelamo <marisolrocioyelamo@gmail.com>
2022-04-05 19:53:43 -0300

    calendario parte marisol

--- api/lib/createWorkDaySecurity.js
+++ api/lib/createWorkDaySecurity.js
@@ -3,6 +3,7 @@ const { WorkDay, Securities } = require("../models");
 const createWorkDay = async (req) => {
   console.log(req.body)
   const workDays = await WorkDay.create(req.body);
+  console.log(workDays)
   const security = await Securities.findOne({
     where: { name: req.body.name },
   });
--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -8,7 +8,6 @@ import { getCalendarOffice } from "../states/calendar";
 import { getBranchName } from "../states/singleBranch";
 import { getAllBranches } from "../states/branches";
 import { useDispatch, useSelector } from "react-redux";
-import { useForm } from "react-hook-form";
 import AssignForm from "./AssignForm";
 
 const CalendarComponent = () => {
@@ -16,66 +15,74 @@ const CalendarComponent = () => {
   const [calendar, setCalendar] = useState([]);
   const branches = useSelector((state) => state.branches);
   const dispatch = useDispatch();
-  const { register, errors, handleSubmit } = useForm();
   useEffect(() => {
     dispatch(getAllBranches());
   }, []);
-
+  let coso = "";
   const onClicke = async (e) => {
-    console.log("DATE MARI", date.getDay(date.toDateString()));
-    const today =
-      date.getFullYear() + "-" + date.getMonth() + "-" + date.getDay();
-    console.log("TODAY", today);
-    const day = today.toString();
-    const coso = await dispatch(getBranchName(e.target.value));
-    const id = coso.payload.id;
-    console.log("ID", id);
-    const calendar = await dispatch(getCalendarOffice({ id, day }));
-    console.log(calendar);
-    setCalendar(calendar);
-    return calendar;
+    coso = await dispatch(getBranchName(e.target.value));
+    console.log(coso);
   };
+  const changeCalendar= async()=>{
 
-  const click = async (e) => {
-    console.log(e.target.value);
-  };
-  /*  const arraySecurities= calendar.payload
- const securities= arraySecurities?.map((security)=>{
-   return (
-     <li key={security.id}>
-       <span>{security.name + " "+ security.wishEntryHour}</span>
-     </li>
-   )
- }) */
+  }
   const onbluer = (e) => {
     return (e.target.style.background = "green");
   };
   const options = branches?.map((branch) => {
     return (
-      <option key={branch.id} id={branch.id} value={branch.name}>
+      <option key={branch.id} id={branch.id} value={branch.name} >
         {branch.name}
       </option>
     );
   });
+  const changeDate = async () => {
+    const id = coso.payload.id;
+    const year = date.getFullYear().toString();
+    const day = date.getDate().toString();
+    const month = date.getMonth().toString();
+    const thisDay = year.concat("-", month, "-", day);
+    const calendar = await dispatch(getCalendarOffice({ id, thisDay }));
 
+    return calendar.payload ? setCalendar(calendar.payload) : [];
+  };
+  const arraySecurities = calendar.securities ? calendar.securities : [];
+  const securities = arraySecurities?.map((security) => {
+    return (
+      <li key={security.id}>
+        <span>
+          {"name:" +
+            security.name +
+            "  CUIL:" +
+            security.CUIL +
+            "  hora de entrada: " +
+            security.entryHour}
+        </span>
+      </li>
+    );
+  });
+  console.log("ACA HAY ZANAHORIA", securities);
+  console.log("ACA ESTA TODO PAPA", calendar);
   return (
-    <div className="app ">
-      <h1 className="text-center">React Calendar</h1>
-      <div
-        className="calendar-container"
-        style={{ marginLeft: "550px", marginTop: "80px", size: "150px" }}
-        onBlur={onbluer}
-        onClick={click}
-      >
-        <Calendar onChange={setDate} value={date} />
-        {console.log(date, "ACA")}
-      </div>
-      <p className="text-center">
-        <span className="bold">Selected Date:</span> {date.toDateString()}
-      </p>
+    <div className="calendarContainer">
+      <div className="app ">
+        <h1 className="text-center">React Calendar</h1>
+        <div
+          className="calendar-container"
+          style={{ marginLeft: "550px", marginTop: "80px", size: "150px" }}
+          onBlur={onbluer}
+        >
+          <Calendar onChange={setDate} value={date} />
+          <button value={date} onClick={changeDate}>
+            {" "}
+            serach
+          </button>
+          {console.log(date, "ACA")}
+        </div>
+        <p className="text-center">
+          <span className="bold">Selected Date:</span> {date.toDateString()}
+        </p>
 
-      <div className="calendarContainer">
-        <AssignForm style={{ minWidth: "400px" }} />
         <Card
           style={{
             width: "18rem",
@@ -91,19 +98,20 @@ const CalendarComponent = () => {
               <Form.Control
                 type="date"
                 as="select"
-                onChange={onClicke}
+                onChange={onClicke} 
                 variant="outlined"
                 aria-label="Default select example"
               >
-                <option>Open this select menu</option>
+               {/*  <option>Open this select menu</option> */}
                 {options}
               </Form.Control>
             </Card.Subtitle>
-            <Card.Text>{/* securities */}</Card.Text>
+            <Card.Text>{securities}</Card.Text>
             <Card.Link href="#">Card Link</Card.Link>
             <Card.Link href="#">Another Link</Card.Link>
           </Card.Body>
         </Card>
+        <AssignForm style={{ minWidth: "40px" }} />
       </div>
     </div>
   );
